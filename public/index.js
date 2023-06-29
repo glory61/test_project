5da8e8679e556326b13fe75e733e3ef30ad8f0f9
glory61 <romkarom29@gmail.com>
2023-06-29 19:37:37 +0300

    v1.1

--- public/index.js
+++ public/index.js
@@ -37,10 +37,7 @@ wss.on('connection', (ws) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-const router=require ('./table.js')
-app.use('/', router);
-router.use('/table', require('./table.js'));
-
+app.use('/table', require('./table.js'));
 
 
 
